5e85360a47940cd601bdb77c69b984852279f8ce
ThePhyrus <romaisgood@yandex.ru>
2022-07-30 00:45:44 +0300

    Урок 7. 00:29:00 НЕРАБОЧИЙ КОММИТ!!! Почему-то не получается даже просто повторить код урока (видимо, пора спать). Шрифт из папки assets не устанавливается для TextView. Попробую на PictureOfTheDay....

--- app/src/main/java/roman/bannikov/workwithtextmateriallesson7/MainActivity.kt
+++ app/src/main/java/roman/bannikov/workwithtextmateriallesson7/MainActivity.kt
@@ -1,14 +1,18 @@
 package roman.bannikov.workwithtextmateriallesson7
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import roman.bannikov.workwithtextmateriallesson7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //todo HW бегущая радуга по тексту
     private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.tvText.typeface = Typeface.createFromAsset(this@MainActivity.assets, "fonts/akronim.xml")
     }
 }
