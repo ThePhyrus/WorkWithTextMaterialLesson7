680b61bf8fb1d30e108117b34e51841ebeb4e4d6
ThePhyrus <romaisgood@yandex.ru>
2022-07-29 20:29:22 +0300

    Урок 7. Подключил viewBinding

--- app/src/main/java/roman/bannikov/workwithtextmateriallesson7/MainActivity.kt
+++ app/src/main/java/roman/bannikov/workwithtextmateriallesson7/MainActivity.kt
@@ -2,10 +2,13 @@ package roman.bannikov.workwithtextmateriallesson7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import roman.bannikov.workwithtextmateriallesson7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
