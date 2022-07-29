69bb142cda64f9811dae6e338b95b1b42d954961
ThePhyrus <romaisgood@yandex.ru>
2022-07-29 20:25:34 +0300

    Initial commit

---
+++ app/src/main/java/roman/bannikov/workwithtextmateriallesson7/MainActivity.kt
@@ -0,0 +1,11 @@
+package roman.bannikov.workwithtextmateriallesson7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
